fdcef7eab5af745b3ccbc269b4f76fccad695c5f
Abdul <abmalik1229@gmail.com>
2022-03-15 13:01:35 -0400

    Runner and interface for quotient

--- Runner.java
+++ Runner.java
@@ -3,8 +3,14 @@ public class Runner{
 
 	public Runner(){
 
+		Quotient q =  (x, y) -> System.out.printf("%.3f%n", x/y);
+        q.remainder(200, 23);
 
 	}
+
+	interface Quotient{
+        void remainder(float x, float y);
+    }
 
 
 	public static void main(String[] args)
